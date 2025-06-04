0e8ea5e20148e86090d4fe8dc3d2cd952c6e189f
TableStar <afraroad@gmail.com>
2025-06-04 12:48:13 +0700

    fix: added create user and getuserbyid controller func

--- backend-api/controllers/users_controller.go
+++ backend-api/controllers/users_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"net/http"
 	"react-go/backend-api/database"
+	"react-go/backend-api/helpers"
 	"react-go/backend-api/models"
 	"react-go/backend-api/structs"
 
@@ -18,5 +19,72 @@ func GetUsers(c *gin.Context) {
 		Success: true,
 		Message: "Lists Data Users",
 		Data:    users,
+	})
+}
+
+func GetUserById(c *gin.Context) {
+	id := c.Param("id")
+
+	var user models.User
+
+	if err := database.DB.First(&user, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, structs.ErrorResponse{
+			Success: false,
+			Message: "User not Found",
+			Errors:  helpers.TranslateErrMsg(err),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, structs.SuccessResponse{
+		Success: true,
+		Message: "User Found",
+		Data: structs.UserResponse{
+			Id:        user.Id,
+			Name:      user.Name,
+			Username:  user.Username,
+			Email:     user.Email,
+			CreatedAt: user.CreatedAt.Format("2006-01-02 15:04:05"),
+			UpdatedAt: user.UpdatedAt.Format("2006-01-02 15:04:05"),
+		},
+	})
+}
+
+func CreateUser(c *gin.Context) {
+	var req = structs.UserCreateRequest{}
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, structs.ErrorResponse{
+			Success: false,
+			Message: "Validation Errors",
+			Errors:  helpers.TranslateErrMsg(err),
+		})
+		return
+	}
+
+	user := models.User{
+		Name:     req.Name,
+		Username: req.Username,
+		Email:    req.Email,
+		Password: helpers.HashPass(req.Password),
+	}
+	if err := database.DB.Create(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, structs.ErrorResponse{
+			Success: false,
+			Message: "Failed to create user",
+			Errors:  helpers.TranslateErrMsg(err),
+		})
+		return
+	}
+	c.JSON(http.StatusCreated, structs.SuccessResponse{
+		Success: true,
+		Message: "User created successfully",
+		Data: structs.UserResponse{
+			Id:        user.Id,
+			Name:      user.Name,
+			Username:  user.Username,
+			Email:     user.Email,
+			CreatedAt: user.CreatedAt.Format("2006-01-02 15:04:05"),
+			UpdatedAt: user.UpdatedAt.Format("2006-01-02 15:04:05"),
+		},
 	})
 }
--- backend-api/routes/routes.go
+++ backend-api/routes/routes.go
@@ -13,6 +13,8 @@ func SetupRouter() *gin.Engine {
 	router.POST("/api/register", controllers.Register)
 	router.POST("/api/login", controllers.Login)
 	router.GET("/api/users", middlewares.AuthMiddleware(), controllers.GetUsers)
+	router.POST("/api/users", middlewares.AuthMiddleware(), controllers.CreateUser)
+	router.GET("/api/users/:id", middlewares.AuthMiddleware(), controllers.GetUserById)
 
 	return router
 }
