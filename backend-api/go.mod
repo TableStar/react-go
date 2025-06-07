3997b530f4e81e44ce9705b3602d55b016e4da56
TableStar <afraroad@gmail.com>
2025-06-07 13:18:13 +0700

    feat: added delete user controller and cors

--- backend-api/go.mod
+++ backend-api/go.mod
@@ -3,6 +3,7 @@ module react-go/backend-api
 go 1.23.1
 
 require (
+	github.com/gin-contrib/cors v1.7.5
 	github.com/gin-gonic/gin v1.10.0
 	github.com/go-playground/validator/v10 v10.26.0
 	github.com/golang-jwt/jwt/v5 v5.2.2
@@ -27,6 +28,7 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
--- backend-api/routes/routes.go
+++ backend-api/routes/routes.go
@@ -4,11 +4,19 @@ import (
 	"react-go/backend-api/controllers"
 	"react-go/backend-api/middlewares"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"*"},
+		AllowMethods:  []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:  []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders: []string{"Content-Length"},
+	}))
 
 	router.POST("/api/register", controllers.Register)
 	router.POST("/api/login", controllers.Login)
@@ -16,6 +24,7 @@ func SetupRouter() *gin.Engine {
 	router.POST("/api/users", middlewares.AuthMiddleware(), controllers.CreateUser)
 	router.GET("/api/users/:id", middlewares.AuthMiddleware(), controllers.GetUserById)
 	router.PUT("/api/users/:id", middlewares.AuthMiddleware(), controllers.UpdateUser)
+	router.DELETE("/api/users/:id", middlewares.AuthMiddleware(), controllers.DeleteUser)
 
 	return router
 }
