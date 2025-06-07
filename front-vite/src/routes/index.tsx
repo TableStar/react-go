ce308e951fea3691857d54d9ed30b7657c6ce488
TableStar <afraroad@gmail.com>
2025-06-07 21:11:04 +0700

    feat: added routes and pages

---
+++ front-vite/src/routes/index.tsx
@@ -0,0 +1,40 @@
+import { Routes, Route, Navigate } from "react-router";
+import Register from "../pages/auth/register";
+import Login from "../pages/auth/login";
+import Home from "../pages/home";
+import { useAuthContext } from "../hooks/useAuthContext";
+
+export default function AppRoutes() {
+  const { isAuthenticated } = useAuthContext() ?? false;
+
+  return (
+    <Routes>
+      {/* route "/" */}
+      <Route path="/" element={<Home />} />
+
+      {/* route "/register" */}
+      <Route
+        path="/register"
+        element={
+          isAuthenticated ? (
+            <Navigate to="/admin/dashboard" replace />
+          ) : (
+            <Register />
+          )
+        }
+      />
+
+      {/* route "/login" */}
+      <Route
+        path="/login"
+        element={
+          isAuthenticated ? (
+            <Navigate to="/admin/dashboard" replace />
+          ) : (
+            <Login />
+          )
+        }
+      />
+    </Routes>
+  );
+}
